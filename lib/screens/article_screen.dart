de2fe8c3fc38bb6d90ca8704b86e776bbad7677f
nik25s <nishantsuthar2002@gmail.com>
2023-09-10 01:05:47 +0530

    first commit

---
+++ lib/screens/article_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ArticleScreen extends StatefulWidget {
+  const ArticleScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ArticleScreen> createState() => _ArticleScreenState();
+}
+
+class _ArticleScreenState extends State<ArticleScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
